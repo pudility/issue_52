7b420b118593a43172da7830284b9ceba1150a2b
zoe <z.zoelec2@gmail.com>
2017-11-16 10:32:43 -0800

    🚀

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { observer } from 'mobx-react'
+
+@observer
+class OrgsPage extends React.Component {
+  render = () =>
+    <div>Hello</div>
+}
+export default OrgsPage
